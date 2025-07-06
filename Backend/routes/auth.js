147fff6e06d7264f7748aabdf9a2f874efbc7b0d
harrish ragavan <163574184+harrishragavan@users.noreply.github.com>
2025-07-06 20:53:04 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -15,7 +15,7 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    res.redirect(`http://localhost:5173/login/success?token=${token}`);
+    res.redirect(`https://todo-app-frontend-ssrp.onrender.com/login/success?token=${token}`);
   }
 );
 
